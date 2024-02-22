14fe42e26843a99d99b0bd00a0bd6f85823c8908
ricardo <diazbarronricardojairo@gmial.com>
2024-02-21 23:40:40 -0400

    librerias junit pruebas 2

--- src/test/java/org/example/PalindromoTest.java
+++ src/test/java/org/example/PalindromoTest.java
@@ -1,21 +1,38 @@
 package org.example;
-
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class PalindromoTest {
+    @Test
+    void esPalindromo_radar() {
+        assertTrue(new Palindromo("radar").calcularPalindromo());
+    }
+    @Test
+    void esPalindromo_level() {
+        assertTrue(new Palindromo("level").calcularPalindromo());
+    }
+    @Test
+    void esPalindromo_deified() {
+        assertTrue(new Palindromo("deified").calcularPalindromo());
+    }
 
     @Test
-    void calcularPalindromo() {
-        assertAll(
-                () -> assertTrue(new Palindromo("radar").calcularPalindromo(), "radar"),
-                () -> assertTrue(new Palindromo("level").calcularPalindromo(),"level"),
-                () -> assertTrue(new Palindromo("deified").calcularPalindromo(), "deified"),
-                () -> assertTrue(new Palindromo("Able was I ere I saw Elba").calcularPalindromo(), "Able was I ere I saw Elba"),
-                () -> assertFalse(new Palindromo("hello").calcularPalindromo(), "hello"),
-                () -> assertFalse(new Palindromo("world").calcularPalindromo(), "world"),
-                () -> assertFalse(new Palindromo("notpalindrome").calcularPalindromo(), "notpalindrome")
-        );
+    void esPalindromo_fraseConEspacios() {
+        assertTrue(new Palindromo("Able was I ere I saw Elba").calcularPalindromo());
+    }
+
+    @Test
+    void noEsPalindromo_hello() {
+        assertFalse(new Palindromo("hello").calcularPalindromo());
+    }
+
+    @Test
+    void noEsPalindromo_world() {
+        assertFalse(new Palindromo("world").calcularPalindromo());
+    }
+
+    @Test
+    void noEsPalindromo_notpalindrome() {
+        assertFalse(new Palindromo("notpalindrome").calcularPalindromo());
     }
 }
